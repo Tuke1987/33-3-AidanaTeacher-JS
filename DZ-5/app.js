731508c51775f3477f68e9926bc69fb362cb9d73
Туке Табалдиев <elik@example.com>
2024-01-29 18:50:15 +0600

    DZ5 sorry

---
+++ DZ-5/app.js
@@ -0,0 +1,45 @@
+var tags = ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1', 'p', 'h1', 'ol', 'br']
+var num = 0
+
+var sorted = tags.sort(function  (t1, t2) {
+    return(t2 - t1)
+})
+console.log(sorted.length)
+
+function firstNameLastMidName (f, l, m){
+    const convertedName = firstNameLastMidName.map
+}
+const fLMName = fLMName1.
+
+// const lastName = prompt('Введите свою фамилию')
+// const firstA = lastName[0]
+// const firstName = prompt('Введите свое имя')
+// const midName = prompt('Введите свое отчество')
+// const firstB = midName[0]
+//
+// switch (firstName){
+//     case firstName:
+//         console.log(firstName)
+// }
+//
+// switch (lastName){
+//     case lastName:
+//         console.log(firstA)
+// }
+//
+// switch (midName){
+//     case midName:
+//         console.log(firstB)
+// }
+
+
+// 3) Напишите функцию, куда может прилетать неограниченный список названия книг. Функция должна выводить только те книги,
+//     в названиях которых есть буква «у» и отдельным списком уже все остальные книги. Используйте методы массива
+
+
+var = books = (a)  => {
+    if (a === 'THE FELLOWSHIP OF THE RING BY J.R.R. TOLKIEN'){
+        console.log(a)
+    }
+}
+books('THE VOYAGE OF THE DAWN TREADER BY C.S. LEWIS')
